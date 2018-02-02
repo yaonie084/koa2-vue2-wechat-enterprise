62dad9c06cdfaa0704cb4b8a9747f879fadec1cd
ethan <yaonie084@gmail.com>
2018-02-02 15:31:33 +0800

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const router = require('koa-router')()
+const home = require('../controllers/home');
+
+router.get('/', home.index);
+router.post('/api/auth', home.auth);
+router.get('/api/get-user-info', home.getUserInfo);
+
+module.exports = router;
